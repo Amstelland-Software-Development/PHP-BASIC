ed4262a9754d66b368648948bb6dd0915e0c53f4
David van Oudheusden <40887850+MeesterDaaf@users.noreply.github.com>
2020-06-02 13:31:53 +0200

    Update producten.php

--- opdracht/producten.php
+++ opdracht/producten.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Title</title>
-  </head>\
+  </head>
   <body>
     <form action="producten.php" method="get"><!-- dit laten staan! -->
 
